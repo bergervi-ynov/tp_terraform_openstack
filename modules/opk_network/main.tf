27ddddccf9c3850e4f9af50dc9312d70f89851b5
VincentBergerOab <125469447+bergervi-ynov@users.noreply.github.com>
2023-07-05 17:20:06 +0200

    change network

--- modules/opk_network/main.tf
+++ modules/opk_network/main.tf
@@ -15,6 +15,6 @@ resource "openstack_networking_network_v2" "vm_network" {
 resource "openstack_networking_subnet_v2" "vm_subnet" {
   name            = "example-subnet"
   network_id      = openstack_networking_network_v2.vm_network.id
-  cidr            = "10.0.0.0/24"
+  cidr            = "192.168.1.0/24"
   ip_version      = 4
 }
