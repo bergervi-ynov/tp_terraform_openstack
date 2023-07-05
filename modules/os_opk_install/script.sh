b7e22adadf1537a10b9cbb3981f6ac782cacde68
VincentBergerOab <125469447+bergervi-ynov@users.noreply.github.com>
2023-07-05 23:36:19 +0200

    Update script.sh

--- modules/os_opk_install/script.sh
+++ modules/os_opk_install/script.sh
@@ -4,6 +4,6 @@ ADMIN_PASSWORD=StrongAdminSecret
 DATABASE_PASSWORD=$ADMIN_PASSWORD
 RABBIT_PASSWORD=$ADMIN_PASSWORD
 SERVICE_PASSWORD=$ADMIN_PASSWORD
-HOST_IP=9.10.93.4
+HOST_IP=$(HOST_IP:-$(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p'))
 SERVICE_HOST=$HOST_IP
 OS_AUTH_URL=http://$SERVICE_HOST:5000/v3.0">> /opt/stack/devstack/local.conf
