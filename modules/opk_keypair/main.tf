cf32e36751dbc299a0e52f6e773d2c9e41d5d8bc
VincentBergerOab <125469447+bergervi-ynov@users.noreply.github.com>
2023-07-06 11:47:19 +0200

    Update main.tf

--- modules/opk_keypair/main.tf
+++ modules/opk_keypair/main.tf
@@ -13,14 +13,14 @@ resource "openstack_compute_keypair_v2" "my-keypair" {
 
 resource "null_resource" "stdout_key_private" {
   provisioner "local-exec" {
-    command = "echo ${openstack_compute_keypair_v2.my-keypair.private_key} > /home/$USER/.ssh/terraform_rsa"
+    command = "echo '${openstack_compute_keypair_v2.my-keypair.private_key}' > /home/$USER/.ssh/terraform_rsa"
     
   }
   
 }
 resource "null_resource" "stdout_key_public" {
   provisioner "local-exec" {
-    command = "echo ${openstack_compute_keypair_v2.my-keypair.public_key} > /home/$USER/.ssh/terraform_rsa.pub"
+    command = "echo '${openstack_compute_keypair_v2.my-keypair.public_key}' > /home/$USER/.ssh/terraform_rsa.pub"
     
   }
   
