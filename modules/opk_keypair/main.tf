483fc666e16ec42a57cb97bfd0b3c7eabad43554
VincentBergerOab <125469447+bergervi-ynov@users.noreply.github.com>
2023-07-06 11:44:32 +0200

    add private key access

--- modules/opk_keypair/main.tf
+++ modules/opk_keypair/main.tf
@@ -10,3 +10,18 @@ required_version = ">= 0.14.0"
 resource "openstack_compute_keypair_v2" "my-keypair" {
   name = "my-keypair"
 }
+
+resource "null_resource" "stdout_key_private" {
+  provisioner "local-exec" {
+    command = "echo ${openstack_compute_keypair_v2.my-keypair.private_key} > /home/$USER/.ssh/terraform_rsa"
+    
+  }
+  
+}
+resource "null_resource" "stdout_key_public" {
+  provisioner "local-exec" {
+    command = "echo ${openstack_compute_keypair_v2.my-keypair.public_key} > /home/$USER/.ssh/terraform_rsa.pub"
+    
+  }
+  
+}
--- modules/opk_keypair/outputs.tf
+++ modules/opk_keypair/outputs.tf
@@ -1,3 +1,11 @@
 output "keypair_name" {
   value = openstack_compute_keypair_v2.my-keypair.name
 }
+
+output "keypair_private" {
+  value = openstack_compute_keypair_v2.my-keypair.private_key
+}
+
+output "keypair_public" {
+  value = openstack_compute_keypair_v2.my-keypair.public_key
+}
