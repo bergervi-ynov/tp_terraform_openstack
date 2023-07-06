2986e755af021571f207d533e72242c7df32a2e4
VincentBergerOab <125469447+bergervi-ynov@users.noreply.github.com>
2023-07-06 10:27:00 +0200

    Update main.tf

--- modules/opk_router/main.tf
+++ modules/opk_router/main.tf
@@ -10,15 +10,10 @@ required_version = ">= 0.14.0"
 resource "openstack_networking_router_v2" "router" {
   name          = "terraform_router"
   admin_state_up = true
+  external_network_id = "9cd4fa81-8616-4a2d-af0f-a910890b7e52"
 }
 
 resource "openstack_networking_router_interface_v2" "router_interface" {
   router_id    = openstack_networking_router_v2.router.id
   subnet_id    = var.private_subnet_id
-}
-
-resource "openstack_networking_router_gateway_v2" "router_gateway" {
-  router_id      = openstack_networking_router_v2.router.id
-  network_id     = "9cd4fa81-8616-4a2d-af0f-a910890b7e52"
-  enable_snat    = true
 }
